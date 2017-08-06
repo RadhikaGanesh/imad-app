9c6e5275424974a581865605b44c1a42899c40d7
RadhikaGanesh <radhikag995@gmail.com>
2017-08-07 00:03:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: "Article-one",
+    header:"Article-One",
+    date:"6th Aug 2017",
+    content:`  <p>
+                    This is the content of article one.  This is the content of article one. This is the content of article one.  
+                </p>
+                <p>This is the content of article one. This is the content of article one. This is the content of article one. content of article one. This is the content of article one.  This is the content of article one. This is the content of article one.  This is the content of article one. This is the content of article one. This is the content of article one. content of article one. 
+                </p> `
+    
+};
+
+function createTemplate(data) {
+    var title=data.title;
+    var header= data.header;
+    var date= data.date;
+    var content= data.content;
+var htmlTemplate = `<html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        
+        <meta name="viewport" content="width-device-width,initial-scale-1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+    <body>
+        <div class= "container">
+            <div>
+                <a href="/"> Home </a>
+            </div>
+            <br/>
+           ${header}
+           <div>
+                       ${date}
+                 </div>
+            <div>
+                ${content}
+            </div>
+        </div>
+    </body>
+    
+    
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+  res.send(createTemplate(articleOne)); 
 });
 
 app.get('/article-two', function (req, res) {
