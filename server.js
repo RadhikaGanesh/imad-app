3d821a4ed3335f5b563beda35cc48de07fc35fe5
RadhikaGanesh <radhikag995@gmail.com>
2017-08-06 22:38:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,20 +14,13 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
 
 app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
 });
 
-app.get('/article-two', function (req, res) {
-   res.send("Article-two requesting will be served here"); 
-});
-
-app.get('/article-three', function (req, res) {
-   res.send("Article-three requesting will be served here"); 
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
