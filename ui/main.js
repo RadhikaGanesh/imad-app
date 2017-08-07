c27e82d4422e508e8e7b2fae998143864a848893
RadhikaGanesh <radhikag995@gmail.com>
2017-08-07 11:07:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element= document.getElementById('main-text');
 element.innerHTML='Hi! Radhika';
 //move image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    margiLeft= MarginLeft + 10;
+    img.style.marginleft= marginLeft + 'px';
+}
 
 img.onclick = function() {
-    img.style.marginLeft='200px';
+    var interval = setInterval(moveRight,100);
+    img.style.marginLeft='500px';
 };
