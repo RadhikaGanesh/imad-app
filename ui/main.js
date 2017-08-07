6ab0fe80125a653e9d5df8f751e520ce1a4de87a
RadhikaGanesh <radhikag995@gmail.com>
2017-08-07 11:13:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,9 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
     marginLeft= marginLeft + 10;
-    img.style.marginleft= marginLeft + 'px';
+    img.style.marginLeft= marginLeft + 'px';
 }
 
 img.onclick = function() {
     var interval = setInterval(moveRight,100);
-    img.style.marginLeft='500px';
 };
